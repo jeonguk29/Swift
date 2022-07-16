3a51d92e7539fb76373a47bd0612a1dc13c907c1
woogie <jeonguk29@naver.com>
2022-07-16 23:47:12 +0900

    swift 기본 데이터 타입 : 자료형

---
+++ Swift_basic_grammar_with_yagom/data_types.swift
@@ -0,0 +1,63 @@
+/* 기본 데이터 타입 */
+
+import UIKit
+
+// Swift의 기본 데이터 타입
+// Bool, Int, UInt, Float, Double, Character, String
+
+// Bool
+var someBool: Bool = true
+someBool = false
+// someBool = 0 // 컴파일 오류발생
+// someBool = 1 // 컴파일 오류발생
+
+// Int
+var someInt: Int = -100
+// someInt = 100.1 // 컴파일 오류발생
+
+// UInt
+var someUInt: UInt = 100
+// someUInt = -100 // 컴파일 오류발생
+// someUInt = someInt // 컴파일 오류발생
+
+// Float
+var someFloat: Float = 3.14
+someFloat = 3
+
+// Double
+var someDouble: Double = 3.14
+someDouble = 3
+// someDouble = someFloat // 컴파일 오류발생
+
+// Character
+var someCharacter: Character = "🇰🇷"
+someCharacter = "😄"
+someCharacter = "가"
+someCharacter = "A"
+// someCharacter = "하하하" // 컴파일 오류발생
+print(someCharacter)
+
+// String
+var someString: String = "하하하 😄 "
+someString = someString + "웃으면 복이와요"
+print(someString)
+
+// someString = someCharacter // 컴파일 오류발생
+
+someString = """
+여러줄 문자열을
+사용할 수 있습니다.
+첫 줄에 겹따옴표 세 개,
+마지막 줄에 겹따옴표 세 개를
+사용하면 됩니다.
+"""
+
+someString = """
+겹따옴표 세 개인 줄(첫줄, 끝줄)에서
+줄 바꿈을 하지 않으면 오류가 발생합니다.
+"""
+
+/*
+someString = """오류발생
+오류발생"""
+*/
