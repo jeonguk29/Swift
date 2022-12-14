0a2d06b3eda8fca6045f00ab60fb57c5ec879cd2
woogie <jeonguk29@naver.com>
2022-12-13 21:39:54 -0500

    Storyboard Segue를 이용하여 화면전환 해보기 및 앱 구현 완료

---
+++ iOS_App_Study_with_yagom/day3_AutoLayout_CodeUI_Edit/day1_Adding_Slider_and_HitButton/CreditViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreditViewController.swift
+//  day1_Adding_Slider_and_HitButton
+//
+//  Created by 정정욱 on 2022/11/28.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+   
+    
+    @IBAction func touchUpDismissButton(_ sender:UIButton){
+            dismiss(animated: true, completion: nil)
+    } // 화면에서 사라지게 하구 completion nil즉 아무일도 일어나지 않게 해라
+    
+  
+
+}
