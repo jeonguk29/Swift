8e7ccfbcf4a81e4f07c6c8aa1fb63b17aaf6f1ae
정정욱 <jeonguk29@naver.com>
2023-01-01 10:36:55 -0500

    반복문 연습문제 풀이 : 소수 판별, 재귀함수(스택 오버플로우 주의)

---
+++ Allen_Swift_Master_School/SwiftMaster/7-1-연습문제/7-1-1-exercise.playground/Contents.swift
@@ -0,0 +1,133 @@
+import UIKit
+
+
+/*:
+ # 연습문제
+ */
+/*:
+ 1) 랜덤 문자열 뽑아내는 함수
+ * 문자열을 입력하면 그중 한개의 글자를 랜덤으로 뽑아주는 함수를 만들어 보자!
+ ---
+ */
+// "안녕하세요"  ===> "녕"
+
+func chooseRandomString(_ chars: String) -> String {
+    return String(chars.randomElement()!)
+}
+
+
+
+
+chooseRandomString("안녕하세요")
+
+
+
+
+
+/*:
+ 2) 소수를 판별하는 함수
+ * 소수, 즉, 1과 자기 자신만으로 나누어 떨어지는 1보다 큰 양의 정수를 판별하는 함수를 만들어 보기
+ ---
+ */
+// 소수판별을 for문으로 구현하기
+
+var num = 97
+
+var isPrime: Bool = true   // 깃발. : 처음에는 소수야 라고 믹리 정해놓고 시작하는 것임
+
+
+for i in 2..<num {
+    
+    if num % i == 0 {
+        isPrime = false // 나누어 떨어지면 소수가 아님
+        break // 반복문을 끝내버림
+    }
+}
+
+
+if isPrime {
+    print("소수입니다.")
+} else {
+    print("소수가 아닙니다.")
+}
+
+
+// 7 은 2,3,4,5,6 으로 딱 나누어지지 않음
+// 9 는 2,3,4,5,6,7,8 로  딱 나누어지지 않음
+// 11도 마찮가지 공통점은 1과 자기 자신과 같은 수를 제외하고 그 아래에 있는 수로 나누어서 떨어지는지 확인만 하면 알 수가 있는것임
+
+
+
+
+// 소수 여부를 알려주는 함수 ==========================
+
+func primeNumberCheck(num: Int) -> Bool {
+    for i in 2..<num {
+        if num % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+
+
+if primeNumberCheck(num: 7) { // 이렇게 bool 값을 반환하는 함수는 조건문에서 바로 사용할 수 있음
+    print("소수입니다.")
+} else {
+    print("소수가 아닙니다.")
+}
+
+
+
+
+// 소수를 판별해서 프린트까지 해주는 함수 =================
+
+func primeNumber(num: Int) {
+    for i in 2..<num {
+        if num % i == 0 {
+            print("소수가 아닙니다.")
+            return
+        }
+    }
+    print("소수입니다.")
+}
+
+
+
+primeNumber(num: 97)
+
+
+
+
+
+
+
+
+//Copyright (c) 2021 we.love.code.allen@gmail.com
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+//distribute, sublicense, create a derivative work, and/or sell copies of the
+//Software in any work that is designed, intended, or marketed for pedagogical or
+//instructional purposes related to programming, coding, application development,
+//or information technology.  Permission for such use, copying, modification,
+//merger, publication, distribution, sublicensing, creation of derivative works,
+//or sale is expressly withheld.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
