386cb8d0c37c6ac7234fee5a9599af756757e2ba
정정욱 <jeonguk29@naver.com>
2023-01-09 12:15:28 -0500

    [UIImage] : UIImage 배열타입에 이미지 리터럴을 배열에 담고, 버튼 클릭시 랜덤으로 주사위 이미지 바꾸기

--- Allen_iOS_App_programming/DiceGame/DiceGame/ViewController.swift
+++ Allen_iOS_App_programming/DiceGame/DiceGame/ViewController.swift
@@ -14,14 +14,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var secondImageView: UIImageView!
     
+    //스토리보드랑 연결 되어 있지 않아도 코드 작성 가능함
     
+    //[UIImage] 이렇게 타입을 줘야 이미지 리터럴을 배열에 담을 수 있음
+    // #imageLiteral() 이미지 리터럴
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6") ]
+    
+    
+    // 앱의 화면에 들어오면 가장 먼저 실행되는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstImageView.image = diceArray[1]
+        secondImageView.image = diceArray[1]
+      
     }
 
     @IBAction func rollButtonTapped(_ sender: UIButton) {
+      // 첫번째 이미지뷰의 이미지를 랜덤으로 변경
+    
+        // firstImageView.image = // 랜덤 이미지
+        firstImageView.image = diceArray.randomElement()
+        
+        
+        // 두번째 이미지뷰의 이미지를 랜덤으로 변경
+        secondImageView.image = diceArray.randomElement()
     }
+    
+    
+    
     
 }
 
