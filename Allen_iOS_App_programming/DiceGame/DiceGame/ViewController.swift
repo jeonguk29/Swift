8587564595d1bd647ae3c76a0a6471bf3b68fbda
정정욱 <jeonguk29@naver.com>
2023-01-09 11:47:34 -0500

    주사위 앱 화면 오토레이아웃 설정완료

---
+++ Allen_iOS_App_programming/DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 정정욱 on 2023/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+    }
+    
+}
+
