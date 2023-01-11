1323a4d6414a8a1438380c9985576e02b7d7b77d
정정욱 <jeonguk29@naver.com>
2023-01-11 05:01:49 -0500

    UpDownGame 코드구현 완료 및 앱완성 * 또 다른 방법 제시  숫자레이블에 있는 문자열 가져와서 (옵셔널 벗기기)  변수를 따로 만들어 저장하지 않음*

--- Allen_iOS_App_programming/UpDownGame/UpDownGame/ViewController.swift
+++ Allen_iOS_App_programming/UpDownGame/UpDownGame/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
     
     // 컴퓨터가 랜덤으로 숫자 선택
     var comNumber = Int.random(in: 1...10) // 이코드가 viewDidLoad() 에 있으면 안되는 이유 viewDidLoad() 또한 함수라 스택에 올라가서 사라지면 끝남 우리는 이 값을 앱 전역에서 가지고 놀기위해 여기다 선언 하는것임
-    
-    var myNumber: Int = 1 // 1을 넣은 이유는 0으로 초기화 하고 사용자가 Select 버튼을 눌렀을때 오류가 날 수 있기 때문임
-    
+
+//    var myNumber: Int = 1 // 1을 넣은 이유는 0으로 초기화 하고 사용자가 Select 버튼을 눌렀을때 오류가 날 수 있기 때문임
+//
     
     // 앱의 화면에 들어오면 가장 처음에 실행 되는 함수
     override func viewDidLoad() {
@@ -37,15 +37,21 @@ class ViewController: UIViewController {
         numberLabel.text = numString
         // 3) 선택한 숫자를 변수에다가 저장 (선택)
         
-        guard let num = Int(numString) else { return }
-        
-        myNumber = num // 가드랫 바인딩 장점 아래서 바로 사용 가능함
-        
+//        guard let num = Int(numString) else { return }
+//
+//        myNumber = num // 가드랫 바인딩 장점 아래서 바로 사용 가능함
+//
         
     }
     
     
     @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+        // 또 다른 방법 제시
+        // 숫자레이블에 있는 문자열 가져오기(옵셔널 벗기기)
+        guard let myNumSting = numberLabel.text else { return }
+        guard let myNumber = Int(myNumSting) else { return }
+        
         // 1 컴퓨터의 숫자와 내가 선택한 숫자를 비교 UP / Down / Bingo (메인 레이블)
         
         if comNumber > myNumber {
