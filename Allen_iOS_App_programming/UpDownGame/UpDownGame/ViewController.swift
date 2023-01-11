d79d372fbd24f14569e50f5f99519eb1e885de80
정정욱 <jeonguk29@naver.com>
2023-01-11 03:51:51 -0500

    UpDownGame 오토레이아웃 잡기 완료

---
+++ Allen_iOS_App_programming/UpDownGame/UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 정정욱 on 2023/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
