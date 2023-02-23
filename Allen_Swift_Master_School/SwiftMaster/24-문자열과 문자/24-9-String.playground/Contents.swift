d15da93ed5c52934023e013d3b9e5fc32724819c
정정욱 <jeonguk29@naver.com>
2023-02-23 16:25:02 +0900

    문자열 다루기 : 비교하기 (비교 옵션) / 포함여부 및 글자 뽑아내기
    *비교옵션은 OptionSet프로토콜을 채택해서, 배열형식으로 전달가능 (즉 여러개 옵션 전달 가능)
    *비교결과(ComparisonResult) 타입은 보다 구체적인 정보의 열거형(오름차순/내림차순/동일)으로
    리턴하기 때문에 활용도가 높음
    
    ComparisonResult 열거형 타입이해 (비교 결과 타입)
    
    https://zeddios.tistory.com/466
     참고
     문자열을 아스키 코드 값으로 비교하여 같은지 작은지 큰지에 따라 결과를 도출함

---
+++ Allen_Swift_Master_School/SwiftMaster/24-문자열과 문자/24-9-String.playground/Contents.swift
@@ -0,0 +1,212 @@
+import UIKit
+
+/*:
+ ## 문자열 비교하기
+ * 문자열의 단순 비교
+ ---
+ */
+// 비교연산자 (대소문자 구별)
+
+"swift" == "Swift"   // false
+"swift" != "Swift"   // true   ===> 둘의 문자는 다른 것임
+
+
+
+// 크기 비교하기 (유니코드 비교)
+
+"swift" < "Swift"      // false ====> 첫 글자의 (유니코드) 순서를 비교
+"swift" <= "Swift"     // false ====> 소문자가 (유니코드/아스키코드) 더 뒤에 위치
+//"Swift" <= "swift"     // true
+
+/*
+ 😀 첫글자 끼리 유니코드 비교 두번째 글자 끼리 유니코드 비교 반복...
+ */
+
+// 대소문자 무시하고 비교는?
+// (간단하게는 일치시킨 후 비교)
+
+"swift".lowercased() == "Swift".lowercased()
+	
+
+//:> 대소문자무시하고 비교하는 메서드 존재 → caseInsensitiveCompare(문자열)
+var a = "swift"
+var b = "Swift"
+
+
+/*
+ a.caseInsensitiveCompare(b)
+ NSComparisonResult 대소문자 무시하고 비교하는 메서드를 사용하면 결과가 참거짓이 아니라 새로운 타입으로 나옴
+ */
+
+a.caseInsensitiveCompare(b) == ComparisonResult.orderedSame
+
+
+//문자열.caseInsensitiveCompare(<#T##aString: StringProtocol##StringProtocol#>)
+
+
+/**========================================================
+ ComparisonResult 열거형 타입으로 정의  (비교 결과 타입)
+  1) .orderedSame          // 동일
+  2) .orderedAscending     // 오름차순
+  3) .orderedDescending    // 내림차순
+ 
+ - 단순 같은지 틀린지 뿐만아니라, 결과가 오름차순/내림차순인지
+   내림차순인지 알수 있어서 결과값이 활용성이 높고 보다 구체적인 정보 제공가능 ⭐️
+   (다만, 처음 사용하는 입장에서 헷갈릴 수 있으므로 잘 알고
+   사용해야하는 불편함이 있을 수 있음)
+===========================================================**/
+
+var result = a.caseInsensitiveCompare(b)
+// 결과 값이 열거형으로 정의되어 있기 때문에 결과 값을 switch문으로 처리 할 수 있는 것임
+// 3가지로 처리 할 수 있으니 참과 거짓이 주는 정보보다 훨씬 풍부함
+switch result {
+case.orderedAscending:
+    print("오름 차순으로 나옴")
+case.orderedDescending:
+    print("내림 차순으로 나옴")
+case.orderedSame:
+    print("동일한 차순으로 나옴")
+}
+
+/*
+let a2: String = "A"
+let b2: String = "B"
+
+a2.compare(b2) == ComparisonResult.orderedAscending                   // true
+b2.compare(a2) == ComparisonResult.orderedDescending                   // true
+ 
+ 
+ https://zeddios.tistory.com/466
+ 참고
+ 문자열을 아스키 코드 값으로 비교하여 같은지 작은지 큰지에 따라 결과를 도출함
+*/
+/*:
+ ## 문자열 비교 메서드
+ * 문자열에서 일치여부 확인 메서드 사용
+ ---
+ */
+//:> 일치여부 메서드 사용 → "다양한 옵션"적용이 가능해서 비교를 여러가지 방식으로 활용가능
+// 문자열.compare(_:options:range:locale:) ⭐️
+
+
+var name = "Hello, Swift"
+
+
+name.compare("hello", options: [.caseInsensitive]) == .orderedDescending    // 내림차순  ( , ==>  )
+
+
+/*:
+ ---
+ * 문자열 비교 옵션(String.CompareOptions)과 비교 결과
+ ---
+ */
+// String.CompareOptions (비교 옵션)
+
+/**==========================================
+ [String.CompareOptions 구조체]와 내부의 타입 속성들
+ - .caseInsensitive       // 대소문자 무시하고 ⭐️
+ 
+ - .diacriticInsensitive  // 발음구별기호 무시하고
+ - .widthInsensitive      // 글자 넓이 무시하고
+ 
+ - .forcedOrdering        // 강제적 오름차순/내림차순 정렬순 (대소문자 무조건 구별 의미)
+ - .literal               // (유니코드 자체로) 글자그대로
+ - .numeric               // 숫자 전체를 인식해서 비교
+ 
+ - .anchored              // (앞부분부터) 고정시키고 (접두어)
+ - .backwards             // 문자 뒷자리부터
+
+ - .regularExpression     // 정규식 검증 ⭐️
+=============================================**/
+
+
+
+// 옵션 입력 부분
+// OptionSet 프로토콜 채택시, 여러개의 옵션을 배열 형식으로 전달 가능
+// NSString.CompareOptions
+/**==========================================
+ struct CompareOptions : OptionSet   프로토콜 채택
+=============================================**/
+
+/*
+ 중첩타입으로
+ struct A{
+     struct B : CompareOptions{
+         
+     }
+ }
+
+ 이런식으로 구현 되어 있다고 생각하면 됨
+ */
+
+
+// .diacriticInsensitive 발음구별기호 무시하고
+"café".compare("cafe", options: [.diacriticInsensitive]) == .orderedSame
+
+
+// .widthInsensitive 글자 넓이 무시하고
+"ァ".compare("ｧ", options: [.widthInsensitive]) == .orderedSame
+
+
+
+
+// .forcedOrdering 강제적 오름차순/내림차순 정렬순 (대소문자 무조건 구별 의미)
+"Hello".compare("hello", options: [.forcedOrdering, .caseInsensitive]) == .orderedAscending
+
+
+// .numeric 옵션 숫자 전체를 인식해서 비교
+"album_photo9.jpg".compare("album_photo10.jpg", options: [.numeric]) == .orderedAscending
+
+
+// .literal 옵션
+"\u{D55C}".compare("\u{1112}\u{1161}\u{11AB}", options: [.literal]) == .orderedSame
+// "한"(완성형)     "ㅎ+ㅏ+ㄴ"(조합형)
+// : 앞 강의에서 두글자 같다고 이야기 했는데 비교해서 위와 같은 옵션을 주면 다르게 보는것임 글자 그대로 비교 하는 옵션이라
+
+
+
+
+// .anchored 옵션 (앞부분부터) 고정시키고 (접두어)
+if let _ = "Hello, Swift".range(of: "Hello", options: [.anchored]) {   // 범위리턴 ===> 접두어 기능
+    print("접두어 일치")
+}
+
+
+// .anchored 옵션 + .backwards 뒷자리부터 고정 (접미어)
+if let _ = "Hello, Swift".range(of: "Swift", options: [.anchored, .backwards]) {   // ===> 접미어 기능
+    print("접미어 일치")
+}
+
+
+
+
+
+
+
+//Copyright (c) 2021 we.love.code.allen@gmail.com
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+//distribute, sublicense, create a derivative work, and/or sell copies of the
+//Software in any work that is designed, intended, or marketed for pedagogical or
+//instructional purposes related to programming, coding, application development,
+//or information technology.  Permission for such use, copying, modification,
+//merger, publication, distribution, sublicensing, creation of derivative works,
+//or sale is expressly withheld.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
