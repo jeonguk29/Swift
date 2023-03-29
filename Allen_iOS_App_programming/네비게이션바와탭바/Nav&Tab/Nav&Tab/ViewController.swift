88e4163ba2e82ffa8193475f3288b34ccb9cae15
정정욱 <jeonguk29@naver.com>
2023-03-29 16:33:07 +0900

    네비게이션바, 탭바 : 스토리보드로 만들어 보기
    네이게이션 컨트롤러와 탭바 컨트롤러를 삽입하면 기본 뼈대가 되는 컨트롤러가 되어서 연결되어 있는 컨트롤러들 제어함
    네비바와 관련된 컨트롤러는 열릴때 우측으로 슬라이드 처리 되고 상단에 네비바가 연동되어 뒤로 가기가 가능
    이 기능은 push, pop 기능임 스택과 관련 있음 내부적으로

---
+++ Allen_iOS_App_programming/네비게이션바와탭바/Nav&Tab/Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by 정정욱 on 2023/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
