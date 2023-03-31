c32b42d2816e2c38f54bb5bb2a206b28eaf1314c
정정욱 <jeonguk29@naver.com>
2023-03-31 23:15:52 +0900

    테이블뷰 : 코드로 작성하기
    주의!!
            // 셀의 등록과정⭐️⭐️⭐️ (스토리보드 사용시에는 스토리보드에서 자동등록)
            //tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
            // MyTableViewCell.self 는 문법에서 메타타입이라고 했음 구체적 인스턴스를 넣는 것이 아닌 붕어빵 틀자체를 여기다가 넣는 것임 즉
            // 데이터 영역에 있는 타입 인스턴스 자체 넣는 것임 이과정이 필수임
    
            tableView.register(MyTableViewCell.self, forCellReuseIdentifier: "MovieCell")
    
    화면 이동시
    
            // ⭐️ 스토리 보드로 만든게 아니라서 세그웨이가 없음 그래서 persent같은걸 사용하면 됨
            //show(detailVC, sender: nil)
    
            //⭐️ 네비게이션을 사용하고 있기 때문에 정확하게는 이런식으로 구현해야함
            navigationController?.pushViewController(detailVC, animated: true)
    
            /*
             show 자체가 pushViewController를 포함하고 있음 그래서 사실 아이패드 같이 큰 화면인 경우에는
             show 사용시 옆에 화면이 표시됨 아이폰 처럼 화면이 작은경우는 이 자체가 push 방식으로 동작하도록 설계가 되어있음
             */
            }
    
    + 코드로 UI 만들때 SceneDelegate 파일에서 첫 화면이 뜨기전에 네비바를 내장 시켜야함
    ScaneDeleagate.swift파일에서
    
            let naviVC = UINavigationController(rootViewController: ViewController())

---
+++ Allen_iOS_App_programming/테이블뷰/TableViewCode/TableViewCode/Controllers/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  TableViewCode
+//
+//  Created by Allen H on 2022/01/20.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    private let tableView = UITableView() // 테이블 뷰 생성해서 담음
+    
+    // 테이블뷰의 데이터를 표시하기 위한 배열
+    var moviesArray: [Movie] = []
+    
+    // MVC패턴을 위한 데이터 매니저 (배열 관리 - 데이터)
+    var movieDataManager = DataManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupNaviBar()
+        setupTableView()
+        setupDatas()
+        setupTableViewConstraints()
+    }
+    
+    func setupNaviBar() {
+        title = "영화목록"
+        
+        // (네비게이션바 설정관련) iOS버전 업데이트 되면서 바뀐 설정⭐️⭐️⭐️
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()  // 불투명으로
+        appearance.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .systemBlue
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
+
+    func setupTableView() {
+        // 델리게이트 패턴의 대리자 설정
+        tableView.dataSource = self
+        tableView.delegate = self
+        // 셀의 높이 설정
+        tableView.rowHeight = 120
+        
+        // 셀의 등록과정⭐️⭐️⭐️ (스토리보드 사용시에는 스토리보드에서 자동등록)
+        //tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
+        // MyTableViewCell.self 는 문법에서 메타타입이라고 했음 구체적 인스턴스를 넣는 것이 아닌 붕어빵 틀자체를 여기다가 넣는 것임 즉
+        // 데이터 영역에 있는 타입 인스턴스 자체 넣는 것임
+        tableView.register(MyTableViewCell.self, forCellReuseIdentifier: "MovieCell")
+    }
+    
+    func setupDatas() {
+        movieDataManager.makeMovieData() // 일반적으로는 서버에 요청
+        moviesArray = movieDataManager.getMovieData()  // 데이터 받아서 뷰컨의 배열에 저장
+    }
+    
+    // 테이블뷰의 오토레이아웃 설정
+    func setupTableViewConstraints() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    
+    // 1) 테이블뷰에 몇개의 데이터를 표시할 것인지(셀이 몇개인지)를 뷰컨트롤러에게 물어봄
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(#function)
+        return moviesArray.count
+    }
+    
+    // 2) 셀의 구성(셀에 표시하고자 하는 데이터 표시)을 뷰컨트롤러에게 물어봄
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(#function)
+        
+        // (힙에 올라간)재사용 가능한 셀을 꺼내서 사용하는 메서드 (애플이 미리 잘 만들어 놓음)
+        // (사전에 셀을 등록하는 과정이 내부 메커니즘에 존재)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MyTableViewCell
+        
+        cell.mainImageView.image = moviesArray[indexPath.row].movieImage
+        cell.movieNameLabel.text = moviesArray[indexPath.row].movieName
+        cell.descriptionLabel.text = moviesArray[indexPath.row].movieDescription
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    // 셀이 선택이 되었을때 어떤 동작을 할 것인지 뷰컨트롤러에게 물어봄
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 다음화면으로 이동
+        let detailVC = DetailViewController()
+        detailVC.movieData = moviesArray[indexPath.row]
+        // ⭐️ 스토리 보드로 만든게 아니라서 세그웨이가 없음 그래서 persent같은걸 사용하면 됨
+        //show(detailVC, sender: nil)
+
+        //⭐️ 네비게이션을 사용하고 있기 때문에 정확하게는 이런식으로 구현해야함
+        navigationController?.pushViewController(detailVC, animated: true)
+
+        /*
+         show 자체가 pushViewController를 포함하고 있음 그래서 사실 아이패드 같이 큰 화면인 경우에는
+         show 사용시 옆에 화면이 표시됨 아이폰 처럼 화면이 작은경우는 이 자체가 push 방식으로 동작하도록 설계가 되어있음
+         */
+        }
+}
