17b587bd3f6f5b77edbb2bf4fe3da062e610156e
정정욱 <jeonguk29@naver.com>
2023-04-16 23:19:33 +0900

    실제 디바이스가 없을 경우 개발 환경에서 할 수 있는 것과 없는 것을 설명하시오. app의 not running, inactive, active, background, suspended에 대해 설명하시오.

--- Academy123/DemoStudyApp/DemoStudyApp/Models/DataManager.swift
+++ Academy123/DemoStudyApp/DemoStudyApp/Models/DataManager.swift
@@ -15,8 +15,8 @@ class DataManager {
         
         questionDataArray = [
             Question(questionName: "@Main에 대해서 설명하시오.", questionAnswer: "https://green1229.tistory.com/265", questionImportance: UIImage(named: "Image1")),
-            Question(questionName: "앱이 foreground에 있을 때와 background에 있을 때 어떤 제약사항이 있나요?", questionAnswer: "", questionImportance: UIImage(named: "Image1")),
-            Question(questionName: "상태 변화에 따라 다른 동작을 처리하기 위한 앱델리게이트 메서드들을 설명하시오.", questionAnswer: "", questionImportance: UIImage(named: "Image1")),
+            Question(questionName: "실제 디바이스가 없을 경우 개발 환경에서 할 수 있는 것과 없는 것을 설명하시오.", questionAnswer: "https://hyun083.tistory.com/71", questionImportance: UIImage(named: "Image1")),
+            Question(questionName: "app의 not running, inactive, active, background, suspended에 대해 설명하시오.", questionAnswer: "https://aloe-study.tistory.com/202", questionImportance: UIImage(named: "Image1")),
             Question(questionName: "앱이 In-Active 상태가 되는 시나리오를 설명하시오.", questionAnswer: "", questionImportance: UIImage(named: "Image1")),
             Question(questionName: "scene delegate에 대해 설명하시오", questionAnswer: "", questionImportance: UIImage(named: "Image1"))
         ]
