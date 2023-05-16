eb56ab3b203aaa215b2c6ccc344c1b55b55d50d1
정정욱 <jeonguk29@naver.com>
2023-05-16 10:40:50 +0900

    코어데이터를 이용한 ToDo 앱 : 클로저 방식으로 셀에 일어난 동작을 전달 (셀 선택시 이동이 아닌 셀 위에 버튼 선택시 이동하게 구현 )

--- Allen_iOS_App_programming/코어데이터/MyToDoApp/MyToDoApp/Views/ToDoCell.swift
+++ Allen_iOS_App_programming/코어데이터/MyToDoApp/MyToDoApp/Views/ToDoCell.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
+/*
+ 💁
+ 셀을 눌렀을때 다음 화면이 아니라 올라와 있는 버튼 누르면 다음 화면가게 구현을 해야함
+ 이것을 델리게이트 패턴으로 구현해줌
+ */
+
+protocol UpdateButtonProtocol: AnyObject {
+    func updateButtonTapped(cell: ToDoCell)
+}
+
 final class ToDoCell: UITableViewCell {
 
     @IBOutlet weak var backgoundView: UIView!
@@ -15,12 +25,16 @@ final class ToDoCell: UITableViewCell {
     @IBOutlet weak var dateTextLabel: UILabel!
     @IBOutlet weak var updateButton: UIButton!
     
+    //💁
+    //weak var delegate: UpdateButtonProtocol? //해당 프로토콜을 채택한 타입한테 시키는 것임
+    
     // ToDoData를 전달받을 변수 (전달 받으면 ==> 표시하는 메서드 실행) ⭐️
     var toDoData: ToDoData? { // 옵셔널 타입으로 ToDoData을 담을 수 있게 만듬
         didSet {
             configureUIwithData() // 속성 감시자를 통해 데이터 들어오면 셋팅
         }
     }
+    
     
     // (델리게이트 대신에) 실행하고 싶은 클로저 저장
     // 뷰컨트롤러에 있는 클로저 저장할 예정 (셀(자신)을 전달)
@@ -63,5 +77,11 @@ final class ToDoCell: UITableViewCell {
     @IBAction func updateButtonTapped(_ sender: UIButton) {
         // 뷰컨트롤로에서 전달받은 클로저를 실행 (내 자신 ToDoCell을 전달하면서) ⭐️
         updateButtonPressed(self)
+        
+        
+        
+        //delegate?.updateButtonTapped(cell: self)
+        //💁 커스텀 델리게이트로 버튼 눌릴때 동작전달한다면
+        // 이렇게 전달하고 뷰컨에서 채택해서 사용하면 됨
     }
 }
